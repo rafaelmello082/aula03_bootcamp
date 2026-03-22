cc79ba4335e6c1470a7d73c683a7ee563c26508b
rafaelmello082 <rafaelmello082@gmail.com>
2026-03-22 18:08:46 -0300

    adicionado main trilha 01 aula 03

---
+++ main.py
@@ -0,0 +1,17 @@
+"""x = -3
+
+if x <= 0:
+  print("Menor que zero")
+elif x <= 2:
+  print("Menor que dois")
+else:
+  print("More")"""
+
+
+for i in range(2, 6):
+  print(i)
+
+lista_gagarikows = ['Rikows', 'Googoo', 'Racing', 'Arruda']
+
+for item in lista_gagarikows:
+  print(item)
